35cb785b8288a9de74f46fa4ee717680fed7795d
jamil <jamil_hoho@hotmail.com>
2020-05-21 11:38:41 +0300

    Make static folder for all site

--- src/my_blog/settings.py
+++ src/my_blog/settings.py
@@ -120,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+#if you want to make Static folder for all project you can use this code
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,"static"),
+    'D:\Learn_Django\Blogger\src\static',
+]
