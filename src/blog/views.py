39b95e3627ff12e69796614dc89eef2786cc8987
jamil <jamil_hoho@hotmail.com>
2020-05-23 18:46:07 +0300

    add index and home pages with links

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,4 +1,42 @@
 from django.shortcuts import render
 
+posts=[
+    {
+        'title':'التدوينة الأولى',
+        'content':'نص التدوينة الأولى كنص تجريبي',
+        'post_date':'15-3-2020',
+        'author':'Jamil Houhou'
+
+    },
+    {
+        'title':'التدوينة الثانية',
+        'content':'نص التدوينة الثانية كنص تجريبي',
+        'post_date':'10-5-2020',
+        'author':'Samir Khamel'
+
+    },
+    {
+        'title':'التدوينة الثالثة',
+        'content':'نص التدوينة الثالثة كنص تجريبي',
+        'post_date':'3-5-2020',
+        'author':'Rabih Mounir'
+
+    },
+    {
+        'title':'التدوينة الرابعة',
+        'content':'نص التدوينة الرابعة كنص تجريبي',
+        'post_date':'20-4-2020',
+        'author':'Jamil Houhou'
+
+    },
+]
+
 def home(request):
-    return render(request,'blog/index.html',{'title':'Home'})
+    context={
+        'title':'الصفحة الرئيسية',
+        'posts':posts
+    }
+    return render(request,'blog/index.html',context)
+
+def about(request):
+    return render(request,'blog/about.html',{'title':'من أنا'})
