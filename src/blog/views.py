9514ee05f389ad072ff9a72997613573207863a5
jamil <jamil_hoho@hotmail.com>
2020-05-24 16:59:21 +0300

    finish post and comment display

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,42 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from .models import Post,Comment
 
-posts=[
-    {
-        'title':'التدوينة الأولى',
-        'content':'نص التدوينة الأولى كنص تجريبي',
-        'post_date':'15-3-2020',
-        'author':'Jamil Houhou'
-
-    },
-    {
-        'title':'التدوينة الثانية',
-        'content':'نص التدوينة الثانية كنص تجريبي',
-        'post_date':'10-5-2020',
-        'author':'Samir Khamel'
-
-    },
-    {
-        'title':'التدوينة الثالثة',
-        'content':'نص التدوينة الثالثة كنص تجريبي',
-        'post_date':'3-5-2020',
-        'author':'Rabih Mounir'
-
-    },
-    {
-        'title':'التدوينة الرابعة',
-        'content':'نص التدوينة الرابعة كنص تجريبي',
-        'post_date':'20-4-2020',
-        'author':'Jamil Houhou'
-
-    },
-]
 
 def home(request):
     context={
         'title':'الصفحة الرئيسية',
-        'posts':posts
+        'posts':Post.objects.all()
     }
     return render(request,'blog/index.html',context)
 
 def about(request):
     return render(request,'blog/about.html',{'title':'من أنا'})
+
+def post_detail(request,post_id):
+    post=get_object_or_404(Post,pk=post_id)
+    comments=post.comment.filter(activate=True)
+    context={
+        'title':post,
+        'post':post,
+        'comments':comments
+    }
+    return render(request,'blog/detail.html',context)
